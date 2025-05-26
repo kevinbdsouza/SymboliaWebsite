04f4afc4dfaa26a010d46b750ec349b0a57e004d
kevinbdsouza <kdsouza1496@gmail.com>
2025-05-26 18:17:03 -0400

    update

--- next.config.js
+++ next.config.js
@@ -1,6 +1,13 @@
 /** @type {import('next').NextConfig} */
+import { initOpenNextCloudflareForDev } from "@opennextjs/cloudflare";
 const nextConfig = {
   reactStrictMode: true,
 }
 
 module.exports = nextConfig
+
+export default {
+  // your existing config
+};
+
+initOpenNextCloudflareForDev();
